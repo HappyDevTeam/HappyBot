07f717c1854089fbeb708c6f0d0449d23032de34
Vincent Huang <vincent_huang_4@sfu.ca>
2024-05-01 16:48:41 -0700

    Added group commands to group up all translation-related commands and implemented a feature that auto-detects non-english texts and translates them to english

--- botcommands/responses.py
+++ botcommands/responses.py
@@ -1,9 +1,14 @@
 from discord.ext import commands
-import botcommands
+from botcommands import translate
 
 
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
+
+    if not translate.is_english(user_input):
+        en_code = list(filter(lambda lang: lang['name'] == 'English', translate.langs))[0]["language"]
+        translated_text = translate.translate_text(en_code, user_input)
+        return translated_text["translatedText"]
 
     if lowered == '':
         return 'Well, you\'re awfully silent...'
