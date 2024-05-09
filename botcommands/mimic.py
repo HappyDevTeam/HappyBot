3774c988abebedf25105015b5d725299de68ccbc
chsoen <22986484+chsoen@users.noreply.github.com>
2024-05-09 13:02:08 -0700

    BOT-15 Model accuracy or dataset is inadequate. Various refactorings to the mimic extension.

--- botcommands/mimic.py
+++ botcommands/mimic.py
@@ -16,65 +16,84 @@ import tensorflow as tf
 from keras.api.models import Sequential
 from keras.api.layers import Dense
 from keras.api.layers import Embedding
+from keras.api.layers import GlobalAveragePooling1D
+from keras.api.layers import Dropout
 from keras.api.layers import Flatten
 from keras.api.optimizers import SGD
+from keras.api.optimizers import Adam
 
 
 class Mimic():
     model: Sequential
     classes: Any
-
+    
     @staticmethod
     def preprocessfeatures(x: list[str]):
-        tokenizer = Tokenizer(num_words=300, filters=' ', oov_token='UNK')
-        tokenizer.fit_on_texts(x)
-        x_full = tokenizer.texts_to_sequences(x)
-        x_full = pad_sequences(x_full, maxlen=200, padding='post')
-        return x_full
+        return tf.convert_to_tensor(x)
 
     @staticmethod
-    def preprocesslabels(y: list[str]):
+    def preprocess(x: list[str], y: list[str], vocab_size: int, max_len: int):
+        vectorize_layer = TextVectorization(
+            max_tokens=vocab_size,
+            output_mode='int',
+            output_sequence_length=max_len,
+            vocabulary=list(set(x))
+        )
         le = LabelEncoder()
         le.fit(y)
+        Mimic.classes = le.classes_
         num_classes = le.classes_.size  # type: ignore
         y_full = le.transform(y)
         y_full = to_categorical(y_full)
-        return y_full, le.classes_, num_classes
+
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y_full, train_size=0.8)
+        return vectorize_layer, num_classes, x_train, x_test, y_train, y_test
+
+    @staticmethod
+    def fitAndEvaluate(epochs, batch_size, x_train, y_train, x_test, y_test):
+        # es = EarlyStopping(monitor='loss', min_delta=0.005, patience=10, verbose=1, mode='auto')
+        print(f"\nFitting fold")
+        Mimic.model.fit(x_train, y_train, epochs=epochs,
+                        shuffle=True, batch_size=batch_size,
+                        verbose=0,
+                        # callbacks=[es]
+                        )
+        print(f"\nTest evaluation")
+        Mimic.model.evaluate(x_test, y_test)
 
     @staticmethod
     def makeModel(x: list[str], y: list[str]):
-        print(x, y)
-        x_full = Mimic.preprocessfeatures(x)
-        y_full, Mimic.classes, num_classes = Mimic.preprocesslabels(y)
-        print(y_full)
-        x_train, x_test, y_train, y_test = train_test_split(
-            x_full, y_full, train_size=0.8)
+        vocab_size = 1000
+        max_len = 30
+        vectorize_layer, num_classes, x_train, x_test, y_train, y_test = Mimic.preprocess(
+            x, y, vocab_size, max_len)
+
         x_train = tf.convert_to_tensor(x_train)
         y_train = tf.convert_to_tensor(y_train)
         x_test = tf.convert_to_tensor(x_test)
         y_test = tf.convert_to_tensor(y_test)
-        print(x_train, x_test, y_train, y_test, sep="\n")
 
-        vocab_size = 1000
-        embedding_dim = 30
+        embedding_dim = 256
         Mimic.model = Sequential([
+            vectorize_layer,
             Embedding(
                 vocab_size, embedding_dim, trainable=True),
             Dense(512, activation='relu'),
-            Flatten(),
-            Dense(512, activation='relu'),
-            Dense(num_classes, activation='sigmoid'),
+            GlobalAveragePooling1D(),
+            Dense(256, activation='relu'),
+            Dense(128, activation='relu'),
+            Dense(64, activation='relu'),
+            Dense(num_classes, activation='softmax'),
         ])
-        opt = SGD(learning_rate=0.000001)
+        opt = Adam(learning_rate=0.001)
+        epochs = 120
+        batch_size = 100
         Mimic.model.compile(loss="categorical_crossentropy",
-                    optimizer="adam", metrics=['accuracy'])
+                            optimizer=opt, metrics=['accuracy'])
 
-        # es = EarlyStopping(monitor='loss', min_delta=0.005, patience=1, verbose=1, mode='auto')
-        Mimic.model.fit(x_train, y_train, epochs=30,
-                shuffle=True, batch_size=30, verbose=2)#type: ignore
-
-        scores = Mimic.model.evaluate(x_test, y_test)
-        print(Mimic.model.metrics_names[0], Mimic.model.metrics_names[1])
+        Mimic.fitAndEvaluate(epochs, batch_size,
+                             x_train, y_train, x_test, y_test)
 
 
 @commands.hybrid_command(name="train")
