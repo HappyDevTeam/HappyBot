90ac3a6b8c18b4c36aea24cb7c3855cb8a5c208a
chsoen <22986484+chsoen@users.noreply.github.com>
2024-05-14 07:49:34 -0700

    Reorganized code to be more modular and neater.

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from discord import Intents, Message
 from discord.ext import commands
 import pathlib
 
-try:
-    from botcommands import translate
-except Exception as e:
-    translate = None
-    print(e)
-
 CURRENT_DIR = pathlib.Path(__file__).parent
 CMDS_DIR = CURRENT_DIR / "botcommands"
 
@@ -20,41 +14,6 @@ TOKEN: Final[str | None] = os.getenv('DISCORD_TOKEN')
 intents: Intents = Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="$", intents=intents)
-
-
-def get_response(user_input: str) -> str | None:
-    lowered: str = user_input.lower()
-
-    if translate is not None:
-        if translate.is_english(user_input) < -0.9:
-            translated_text = translate.translate_text("en", user_input)
-            return translated_text
-
-
-async def send_message(message: Message, user_message: str) -> None:
-    if user_message is None:
-        print('(Message empty because intents not enabled)')
-        return
-    if user_message is None:
-        return
-    response: str | None = get_response(user_message)
-    if response is None or message is None:
-        return
-    await message.channel.send(response)
-
-
-@bot.event
-async def on_message(message: Message) -> None:
-    if message is None:
-        raise Exception("Message instance does not exist")
-    if message.author == bot.user:
-        return
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
 
 
 @bot.event
@@ -76,6 +35,17 @@ async def on_ready() -> None:
         print(f"Synced {len(synced)} command(s)")
     except Exception as e:
         print(e)
+
+
+@bot.hybrid_command(name="sync")
+async def sync(ctx: commands.Context):
+    try:
+        await ctx.send("Syncing...")
+        await bot.tree.sync(guild=ctx.guild)
+        await ctx.send("Synced!")
+    except Exception as e:
+        print(e)
+    return sync
 
 if __name__ == '__main__':
     assert isinstance(TOKEN, str)
